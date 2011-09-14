401172c85676fdbee76f25b3cb4bdd3fc1b3776f
Aleksandar Milovac <aleksandar.milovac@gmail.com>
2011-09-14 22:07:55 +0200

    Dodao komentar u zaglavlju i informaciju o autoru

--- class.vehicles.php
+++ class.vehicles.php
@@ -2,11 +2,20 @@
 
 class Vehicles {
 
+
+/***class Vehicles*******
+*author Aleksandar Milovac, aleksandar.milovac@gmail.com
+*released under GPL v2 licence
+* defines Vehicle object
+*/
+
 protected $speed;
 protected $color;
 protected $gear;
 protected $engineStatus;
 protected $type;
+protected $horsePower;
+
 
 
 
