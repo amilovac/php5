a56878b62c9d6c3a44a2b8428ee542fc017b6b06
Aleksandar Milovac <aleksandar.milovac@gmail.com>
2011-09-22 23:09:45 +0200

    Seter metodi

--- class.vehicles.php
+++ class.vehicles.php
@@ -4,15 +4,15 @@ class Vehicles {
 
 
 /***class Vehicles*******
-*author Aleksandar Milovac, aleksandar.milovac@gmail.com
-*released under GPL v2 licence
-* defines Vehicle object
-*/
+**author Aleksandar Milovac, aleksandar.milovac@gmail.com
+**released under GPL v2 licence
+** defines Vehicle object
+**/
 
 protected $speed;
 protected $color;
 protected $gear;
-protected $engineStatus;
+protected $engineStatus = 'Off';
 protected $type;
 protected $horsePower;
 
@@ -23,17 +23,45 @@ protected $horsePower;
 
 function __construct($vtype){
 	$this->type=$vtype;
+}
+
+// definicija metoda klase, koje su funkcije objekta?
+
+/***** Setter method za paljenje kola
+**enineStatus : On ili Off
+**
+**/
 
 
+function setEngineStatus($engineStatus){
+    $this->engineStatus = $engineStatus;
+
+}
+
+function setSpeed($speed){
+    $this->speed=$speed;
+
+}
+
+function setColor($color){
+    $this->color=$color;
+}
+
+function setGear($gear){
+    $this->gear=$gear;
+    
 }
 
 
 
 
+
+
+
 }
 
-$car = new Vehicles("auto"); 
-var_dump($car);
+//$car = new Vehicles("auto"); 
+//var_dump($car);
 
 
 ?>
