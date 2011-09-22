f8def696242c77a78f4cfe75358859428e91df22
Aleksandar Milovac <aleksandar.milovac@gmail.com>
2011-09-22 23:36:58 +0200

    Dodati i getter i setter metodi

--- class.vehicles.php
+++ class.vehicles.php
@@ -3,7 +3,7 @@
 class Vehicles {
 
 
-/***class Vehicles*******
+/*class Vehicles
 **author Aleksandar Milovac, aleksandar.milovac@gmail.com
 **released under GPL v2 licence
 ** defines Vehicle object
@@ -17,51 +17,93 @@ protected $type;
 protected $horsePower;
 
 /*konstruktor koji setuje tip vozila:kola, kamion....
-*
-*
+**
+**
 */
 
-function __construct($vtype){
-	$this->type=$vtype;
+function __construct($type){
+	$this->type=$type;
 }
 
 // definicija metoda klase, koje su funkcije objekta?
 
-/***** Setter method za paljenje kola
-**enineStatus : On ili Off
+/* Setter method za paljenje kola
+**engineStatus : On ili Off
 **
 **/
 
 
-function setEngineStatus($engineStatus){
-    $this->engineStatus = $engineStatus;
+	function setEngineStatus($engineStatus){
+   		$this->engineStatus = $engineStatus;
 
-}
+	}
 
-function setSpeed($speed){
-    $this->speed=$speed;
+	function setSpeed($speed){
+    		$this->speed = $speed;
 
-}
+	}
 
-function setColor($color){
-    $this->color=$color;
-}
+	function setColor($color){
+    		$this->color = $color;
+	}
 
-function setGear($gear){
-    $this->gear=$gear;
+	function setGear($gear){
+    		$this->gear = $gear;
     
+	}
+
+	function setType($type){
+    		$this->type = $type;
+    
+	}
+	
+	function setHorsepower($horsepower){
+		$this->horsepower = $horsepower;
+
+	}
+
+/*getter metodhs
+**
+**
+*/
+
+
+	function getEngineStatus(){
+   		return $this->engineStatus;
+
+	}
+
+	function getSpeed(){
+    		return $this->speed;
+
+	}
+
+	function getColor(){
+    		return $this->color;
+	}
+
+	function getGear(){
+    		return $this->gear;
+    
+	}
+
+	function getType(){
+    		return $this->type;
+    
+	}
+	
+	function getHorsepower(){
+		return $this->horsepower;
+
+	}
+
+
+
+
+
+
 }
 
-
-
-
-
-
-
-}
-
-//$car = new Vehicles("auto"); 
-//var_dump($car);
 
 
 ?>
